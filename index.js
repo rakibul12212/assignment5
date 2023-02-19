4210496d843de9f0bead175afa0867d8c0cceb9b
Rakib400372 <rakibulislamr83@gmail.com>
2023-02-19 16:57:00 +0600

    triangle calculation

---
+++ index.js
@@ -0,0 +1,16 @@
+//first card button click
+document.getElementById("first-card").addEventListener("click", function() {
+//console.log("clicked");
+
+//get data from html
+const componantName = document.getElementById("first-name").innerText;
+//console.log(componantName);
+const firstInput = document.getElementById("first-input").value;
+//console.log(firstInput)
+const secondInput = document.getElementById("second-input").value;
+//console.log(componantName,firstInput,secondInput)
+const area = 0.5 * (firstInput) * (secondInput)
+console.log(area,componantName,firstInput,secondInput);
+
+
+});
