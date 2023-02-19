a4575f58b85f2969d1ef87553eb5426d4d3ad759
Rakib400372 <rakibulislamr83@gmail.com>
2023-02-19 17:21:52 +0600

    common function

--- index.js
+++ index.js
@@ -9,20 +9,25 @@ const firstInput = document.getElementById("first-input").value;
 //console.log(firstInput)
 const secondInput = document.getElementById("second-input").value;
 //console.log(componantName,firstInput,secondInput)
-const area = 0.5 * (firstInput) * (secondInput)
-console.log(area,componantName,firstInput,secondInput);
+const area = 0.5 * parseFloat(firstInput) * parseFloat(secondInput)
+const convart = (area) / 10000
 
+console.log(componantName,firstInput,secondInput,area,convart);
+
+displaydata(componantName,area,convart);
+
+});
+//common function to display data
+function displaydata(componantName,area,convart){
+    //table show the data
 const container = document.getElementById("table-container");
 const tr = document.createElement("tr")
 tr.innerHTML = `
 <td>${1}</td>
 <td>${componantName}</td>
 <td>${area}</td>
-
-
-
-
+<td>${convart}</td>
 
 `;
 container.appendChild(tr);
-});
+}
